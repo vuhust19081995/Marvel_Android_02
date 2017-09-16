a81746f447a25760319afe3fb8aebc51290ac5d9
vuhust19081995 <nguyenvanvu.bkhn@gmail.com>
2017-09-16 15:09:30 +0700

    init project

---
+++ app/src/main/java/com/framgia/moviedb/screen/main/MainActivity.java
@@ -0,0 +1,14 @@
+package com.framgia.moviedb.screen.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.framgia.moviedb.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
